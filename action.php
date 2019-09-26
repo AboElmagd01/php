aef7e8967ce4bd7845264fee5e0d0222666ceb4a
Abdelrhman Abo-Elmagd <49569548+AboElmagd01@users.noreply.github.com>
2019-09-26 17:02:47 +0200

    Add files via upload

---
+++ action.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$messages = $_POST["messages"];
+
+
+echo "Welcome: " . $name;
+echo "<br>";
+echo "Your mail is: " . $email;
+echo "<br>";
+
+echo "Your phone number is: " . $phone;
+echo "<br>";
+
+echo "Your Messages are: " . $messages;
+
+
+ ?>
